fb89dc0253d0f2c2875007706a551d00490a0920
Anthony Ferrara <ircmaxell@gmail.com>
2019-04-24 10:19:43 -0400

    Finally fix issue with using correct intrinsic version for LLVM 7+

--- lib/LLVM7/Factory.php
+++ lib/LLVM7/Factory.php
@@ -5,13 +5,13 @@ namespace PHPLLVM\LLVM7;
 use PHPLLVM\LLVMAbstract\Builder;
 use PHPLLVM\LLVMAbstract\Context;
 use PHPLLVM\LLVMAbstract\Factory as AbstractFactory;
-use PHPLLVM\LLVMAbstract\Intrinsic;
+use PHPLLVM\LLVMAbstract\Intrinsic as AbstractIntrinsic;
 use PHPLLVM\LLVMAbstract\Module;
 
 
 class Factory extends AbstractFactory {
 
-    public function intrinsic(Context $context, Module $module, Builder $builder): Intrinsic {
+    public function intrinsic(Context $context, Module $module, Builder $builder): AbstractIntrinsic {
         return new Intrinsic($this->llvm, $context, $module, $builder);
     }
 
